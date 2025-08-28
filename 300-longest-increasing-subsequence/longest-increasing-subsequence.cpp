51d1cfede325d65027aae4988fa4cb62dbf9884e
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2025-08-28 14:57:58 +0530

    Time: 544 ms (23.17%) | Memory: 297 MB (6.98%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    vector<int> arr;
-    vector<vector<int>> dp;
-    int n;
+vector<int>arr;
+vector<vector<int>>dp;
+
+int n;
 
     int lengthOfLIS(vector<int>& nums) {
-        arr = nums;
-        n = nums.size();
-        dp = vector<vector<int>>(n, vector<int>(n+1, -1)); // dp[i][prevIdx+1]
-        return rec(0, -1);
+        n=nums.size();
+      
+        dp=vector<vector<int>>(nums.size()+1,vector<int>(nums.size()+1,-1));
+        arr=nums;
+        return rec(0,-1);
+        
     }
+    int rec(int i,int prev){
+        if(i==n){
+            return 0;
+        }
+        
+        if(dp[i][prev+1]!=-1){
+            return dp[i][prev+1];
+       }
+       int ans=rec(i+1,prev);
+        if(   prev==-1 || arr[prev]<arr[i]){
+            ans=max(ans,rec(i+1,i)+1);
 
-    int rec(int i, int prevIdx) {
-        if (i == n) return 0;
-
-        if (dp[i][prevIdx+1] != -1) return dp[i][prevIdx+1];
-
-        // Option 1: skip current element
-        int ans = rec(i+1, prevIdx);
-
-        // Option 2: take current element if valid
-        if (prevIdx == -1 || arr[i] > arr[prevIdx]) {
-            ans = max(ans, 1 + rec(i+1, i));
         }
 
-        return dp[i][prevIdx+1] = ans;
+        return  dp[i][prev+1]= ans;
     }
+
+
 };
