70af0eac698da0367dea2272e7074f14226bb91f
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-01-25 12:53:20 +0530

    Time: 3 ms (40.22%) | Memory: 17.4 MB (22.33%) - LeetSync

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1){
+            return 0;
+        }
+        int ans=1e9;
+        for(int i=0;i<=nums.size()-k;i++){
+            ans=min(ans,nums[i+k-1]-nums[i]);
+            
+            
+        }
+        return ans;
+    }
+};
