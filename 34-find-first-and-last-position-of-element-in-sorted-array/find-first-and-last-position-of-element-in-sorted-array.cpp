518b8a3ebc621362ab2c83d77a5b4d8e5db4acc5
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-01-26 09:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.75%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,54 +1,49 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+
+
+        int l=0;
+        int h=nums.size()-1;
+        int ans1=-1;
+        while(l<=h){
+            long long mid=(l+h)/2;
+            if(target==nums[mid]){
+                ans1=mid;
+                h=mid-1;
+            }
+            else if(nums[mid]>target){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+
+        }
+        int ans2=-1;
+        int s=0;
+        int e=nums.size()-1;
+        while(s<=e){
+            long long mid=(s+e)/2;
+            if(target==nums[mid]){
+                ans2=mid;
+                s=mid+1;
+            }
+            else if(target>nums[mid]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+
+        }
+        vector<int>ans;
+        ans.push_back(ans1);
+        ans.push_back(ans2);
+
+return ans;
+
         
-        return {func1(nums,target),func2(nums,target)};
     }
-    int func1(vector<int>& nums, int target){
-        int n=nums.size();
-        int l=0;
-        int h=n-1;
-        int ans=-1;
-        while(l<=h){
-            int mid=(h-l)/2+l;
-            if(nums[mid]==target){
-                ans=mid;
-                h=mid-1;
-                
-            }
-            else if(target>nums[mid]){
-                l=mid+1;
 
-            }
-            else {
-                h=mid-1;
-
-            }
-        }
-        return ans;
-    }
-     int func2(vector<int>& nums, int target){
-        int n=nums.size();
-        int l=0;
-        int h=n-1;
-        int ans=-1;
-        while(l<=h){
-            int mid=(h-l)/2+l;
-            if(nums[mid]==target){
-                ans=mid;
-                l=mid+1;
-                
-            }
-            else if(target>nums[mid]){
-                l=mid+1;
-
-            }
-            else {
-                h=mid-1;
-
-            }
-        }
-        return ans;
-    }
-    
 };
