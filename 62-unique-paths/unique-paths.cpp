a0bd5d8a9f0b77aab77ca08a49fce5b99a72be91
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-01-28 09:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (28.26%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
 vector<vector<int>>dp;
+int a,b;
     int uniquePaths(int m, int n) {
-    
-        dp.resize(m,vector<int>(n,-1));
-        return rec(m,n,0,0,0);
+        dp=vector<vector<int>>(m+1,vector<int>(n+1,-1));
+        a=m;
+        b=n;
+        return rec(0,0);
         
     }
-    int rec(int m,int n,int i,int j,int ans){
-        //valid condition 
-        if(i>=m || j>=n){
-            return 0;
-        }
-        // base case 
-        if(i==m-1 && j==n-1){
+    int rec(int i,int j){
+        if(i==a-1 && j==b-1){
             return 1;
+        }
+        if(i>=a || j>=b){
+            return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        ans+=rec(m,n,i+1,j,ans)+rec(m,n,i,j+1,ans);
-        dp[i][j]=ans;
-        return ans;
+        int ans=0;
+        ans+=(rec(i+1,j)+rec(i,j+1));
 
+        return dp[i][j]=ans;
     }
 };
