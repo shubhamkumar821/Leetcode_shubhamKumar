4a6692686252801d12cb16b37f9d73491a659a0a
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-01-31 10:26:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.43%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
+   
     bool isValid(string s) {
-        map<char,int> mpp;
-        mpp['[']=1;
-        mpp['(']=2;
-        mpp['{']=3;
-        mpp[']']=-1;
-        mpp[')']=-2;
-        mpp['}']=-3;
-        int n=s.size();
-        stack<int> st;
-        if(n==1){
-            return false;
-        }
-        
-        for(char v:s){
-            int val=mpp[v];
-            if(val>0){
-                st.push(mpp[v]);
-            }
-            else {
-                if(!st.empty() && st.top()+val==0){
-                    st.pop();
-                }
-                else{
-                    return false;
-                    break;
+        stack<char>st;
+        for(auto i:s){
+            
+            if(st.empty()){
+                st.push(i);
 
-                }
+            }
+
+            else if(i==')' && st.top()=='('){
+                 st.pop();
+
+            }
+              else if(i=='}' && st.top()=='{'){
+                 st.pop();
+
+            }
+              else if(i==']' && st.top()=='['){
+                 st.pop();
+
+            }
+            else{
+                st.push(i);
             }
         }
         if(st.empty()){
