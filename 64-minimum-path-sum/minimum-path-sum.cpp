9911cb1cc02829a6a68554b1c7af3ad42d5d6b61
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-02-01 23:27:40 +0530

    Time: 20 ms (5.80%) | Memory: 16.3 MB (40.42%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,54 @@
 class Solution {
 public:
-vector<vector<int>> dp;
-
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        dp.resize(m,vector<int>(n,-1));
-        return rec(m,n,grid,0,0,0);
-        
+        int m = grid.size();
+        int n = grid[0].size();
+
+        // Distance matrix
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+
+        // Min-heap: {cost, {row, col}}
+        priority_queue<
+            pair<int, pair<int,int>>,
+            vector<pair<int, pair<int,int>>>,
+            greater<>
+        > pq;
+
+        // Start from (0,0)
+        dist[0][0] = grid[0][0];
+        pq.push({grid[0][0], {0, 0}});
+
+        int dr[2] = {0, 1}; // right, down
+        int dc[2] = {1, 0};
+
+        while (!pq.empty()) {
+            auto [cost, cell] = pq.top();
+            pq.pop();
+
+            int r = cell.first;
+            int c = cell.second;
+
+            // Early exit if reached destination
+            if (r == m - 1 && c == n - 1)
+                return cost;
+
+            if (cost > dist[r][c]) continue;
+
+            for (int k = 0; k < 2; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr < m && nc < n) {
+                    int newCost = cost + grid[nr][nc];
+
+                    if (newCost < dist[nr][nc]) {
+                        dist[nr][nc] = newCost;
+                        pq.push({newCost, {nr, nc}});
+                    }
+                }
+            }
+        }
+
+        return dist[m-1][n-1];
     }
-   int  rec(int m,int n,vector<vector<int>>& grid,int i,int j,int ans){
-    if(i>=m||j>=n){
-        return 1e9;
-    }
-    if(i==m-1 && j==n-1){
-        return grid[m-1][n-1];
-    }
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-     ans=min(grid[i][j]+rec(m,n,grid,i+1,j,0),grid[i][j]+rec(m,n,grid,i,j+1,0));
-    dp[i][j]=ans;
-    return ans;
-    
-   }
 };
