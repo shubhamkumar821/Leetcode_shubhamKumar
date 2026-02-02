57e12e1e76a7366e986214c0d0fc7f69154b2ebd
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-02-02 13:50:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (75.83%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n<=0){ 
-            return false;
-        }
-        while(n>1){
-            if(n%3==0){
-                n=n/3;
+        if (n <= 0) return false;
+        return func(n);
+    }
 
-            }
-            else if(n%5==0){
-                n=n/5;
-            }
-            else if(n%2==0){
-                n=n/2;
-            }
-            else{
-                return false;
-            }
-        }
-        return n==1;
-        
+    bool func(int x) {
+        if (x == 1) return true;
+
+        if (x % 2 == 0) return func(x / 2);
+        if (x % 3 == 0) return func(x / 3);
+        if (x % 5 == 0) return func(x / 5);
+
+        return false;
     }
 };
