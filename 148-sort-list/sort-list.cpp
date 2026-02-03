7d1e5c061749f45d5f44b972e6dadcb0a06a7cde
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-02-03 10:48:10 +0530

    Time: 55 ms (15.61%) | Memory: 75.5 MB (40.10%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -11,23 +11,58 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(!head){
+        ListNode*slow=head;
+        ListNode*fast=head;
+          if(head == NULL || head ->next == NULL)
             return head;
+        while(fast->next!=nullptr && fast!=nullptr){
+                fast=fast->next;
+                if(fast->next!=nullptr){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
         }
-        vector<int>ans;
-        while(head){
-            ans.push_back(head->val);
-            head=head->next;
-        }
-        sort(ans.begin(),ans.end());
-        ListNode*temp=new ListNode(ans[0]);
-        ListNode*move=temp;
-        for(int i=1;i<ans.size();i++){
-            ListNode*store=new ListNode(ans[i]);
-            move->next=store;
-            move=store;
-        }
-        return temp;
+    ListNode*temp=slow->next;
+    slow->next=nullptr;
+        ListNode*l1=sortList(temp);
+        ListNode*l2=sortList(head);
+        return mergeList(l1,l2);
         
     }
+    ListNode*mergeList(ListNode*l1,ListNode*l2){
+        ListNode*ptr=new ListNode(0);
+        ListNode*curr=ptr;
+
+        while(l1!=nullptr && l2!=nullptr){
+            if(l1->val>=l2->val){
+                 curr->next=l2;
+                 l2=l2->next;
+
+            }
+            else{
+                 curr->next=l1;
+                 l1=l1->next;
+                
+            }
+    curr = curr ->next;
+
+        }
+
+         if(l1 != NULL)
+        {
+            curr -> next = l1;
+            l1 = l1->next;
+        }
+        
+        if(l2 != NULL)
+        {
+            curr -> next = l2;
+            l2 = l2 ->next;
+        }
+        
+        return ptr->next;
+    }
+
+
+    
 };
