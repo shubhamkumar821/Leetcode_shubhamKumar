1fd3e1d56aca8d8c3523ca26bed02b60044dd8f9
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-05-15 19:16:00 +0530

    Time: 4 ms (62.01%) | Memory: 11.4 MB (63.56%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int>st(n,0);
-        int i2=0,i3=0,i5=0;
-        st[0]=1;
+
+        vector<int>ugly(n,0);
+        int p2=0;
+        int p3=0;
+        int p5=0;
+        ugly[0]=1;
 
         for(int i=1;i<n;i++){
-            int nextugly=min({st[i2]*2,st[i3]*3,st[i5]*5});
-            st[i]=nextugly;
-           
-            if(nextugly==st[i2]*2)i2++;
-             if(nextugly==st[i3]*3)i3++;
-              if(nextugly==st[i5]*5)i5++;
+            int a=ugly[p2]*2;
+            int b=ugly[p3]*3;
+            int c=ugly[p5]*5;
+            ugly[i]=min({a,b,c});
+
+            if(ugly[i]==a)p2++;
+            if(ugly[i]==b)p3++;
+            if(ugly[i]==c)p5++;
         }
-        return st[n-1];
+
+        return ugly[n-1];
+        
     }
 };
