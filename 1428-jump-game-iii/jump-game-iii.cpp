ffa002d3a3d61ef2db73076e63408127dc34e533
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-05-17 13:37:47 +0530

    Time: 1 ms (54.71%) | Memory: 40.8 MB (22.31%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+  int n;
+      vector<int> vis;
+    bool canReach(vector<int>& arr, int start) {
+        int maxi=INT_MIN;
+        n=arr.size();
+        for(auto i:arr){
+            maxi=max(maxi,i);
+        }
+        vis.assign(n,0);
+        return rec(start,arr);
+
+        
+    }
+
+    bool rec(int i,vector<int>& arr){
+    
+        if(i<0 || i>=n){
+            return false;
+        }
+            if(arr[i]==0){
+            return true;
+        }
+        if(vis[i]){
+            return false;
+        }
+        vis[i]=1;
+        
+        
+
+        bool check=rec(i+arr[i],arr)||rec(i-arr[i],arr);
+        return check;
+    }
+};
