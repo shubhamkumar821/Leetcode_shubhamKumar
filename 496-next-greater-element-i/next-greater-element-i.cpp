43292d0a491b74792a52ee1252786c7ae8ae2d39
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-08-16 16:14:28 +0530

    Time: 3 ms (40.75%) | Memory: 13 MB (15.54%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+map<int,int>mp;
+stack<int>st;
 
-         vector<pair<int,int>>v;
+vector<int>ans;
+  for(int i=nums2.size()-1;i>=0;i--){
+    if(st.empty()){
+      mp[nums2[i]]=-1;
+    }
+    else{
+        while(!st.empty() && st.top()<nums2[i]){
+            st.pop();
+        }
+    if(!st.empty() && st.top() >nums2[i]){
+        mp[nums2[i]]=st.top();
+    }
+    else if(st.empty()){
+         mp[nums2[i]]=-1;
 
-         for(int i=0;i<nums1.size();i++){
+    }
 
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    v.push_back({nums1[i],j});
-                }
-            }
+    }
+    st.push(nums2[i]);
+  }
 
-         }
-
-         vector<int>ans;
-         for(auto i:v){
-            bool ch=false;
-
-            for(int j=i.second+1;j<nums2.size();j++){
-                if(i.first<nums2[j]){
-                    ans.push_back(nums2[j]);
-                    ch=true;
-                    break;
-                }
-          
-
-                }
-
-                if(!ch){
-                    ans.push_back(-1);
-                }
-            }
-
-              return ans;
-        
-
-         } 
-       
+  for(auto i:nums1){
+    if(mp[i]!=0){
+        ans.push_back(mp[i]);
+    }
     
+  }
+  //reverse(ans.begin(),ans.end());
+  return ans;
+        
+    }
 };
