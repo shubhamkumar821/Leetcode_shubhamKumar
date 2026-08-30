baaa0c74bc2e45da993ed169861d3969c53604f5
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-08-30 20:41:33 +0530

    Time: 20 ms (94.15%) | Memory: 19.5 MB (35.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,51 +1,42 @@
 class Solution {
 public:
+   int dx[4]={1,-1,0,0};
+   int dy[4]={0,0,-1,1};
     int numIslands(vector<vector<char>>& grid) {
-        vector<int> dx={1,0,-1,0};
-        vector<int> dy={0,1,0,-1};
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<bool>> st(m,vector<bool>(n,false));
-        int cnt=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-           if(!st[i][j] && grid[i][j]=='1')
-           {
-              cnt++;
-              dfs({i,j},st,dx,dy,grid,m,n);
-           }
+        queue<pair<int,int>>q;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int no_island=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+              
+               if(grid[i][j]=='1' && vis[i][j]==0){
+                  q.push({i,j});
+                vis[i][j]=1;
+                no_island++;
+                while(!q.empty()){
+                    auto [x,y]=q.front();
+                    q.pop();
+
+                    for(int i=0;i<4;i++){
+                        int X=x+dx[i];
+                        int Y=y+dy[i];
+                        if(X<0 || Y <0 || X>=n || Y>=m )continue;
+                        if(grid[X][Y]=='0')continue;
+                          if(grid[X][Y]=='1' && vis[X][Y]==0){
+                            vis[X][Y]=1;
+                            q.push({X,Y});
+                          }
+
+
+                    }
+                }
+               }
+
             }
         }
-        return cnt;
-    }
-    void dfs(pair<int,int> k,vector<vector<bool>>& st,vector<int>&dx,vector<int>&dy,vector<vector<char>> & grid,int m,int n)
-    {
-        int a=k.first,b=k.second;
-        st[a][b]=true;
-        for(int i=0;i<4;i++)
-        {
-            if(check( a+dx[i],b+dy[i],m,n))
-            {
-              if(!st[a+dx[i]][b+dy[i]] && grid[a+dx[i]][b+dy[i]]=='1')
-              {
-                  dfs({a+dx[i],b+dy[i]},st,dx,dy,grid,m,n);
-              }
-            }
-        }
-
-    }
-    bool check(int x,int y,int m,int n)
-    {
-        if(x<0||x>=m || y<0 || y>=n)
-        {
-            return false;
-        }
-        else 
-        {
-          return true;
-        }
-
+        return no_island;
+        
     }
 };
