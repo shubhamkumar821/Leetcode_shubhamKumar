225ba088ce97b0cbf2bb59719b6415ed3bc9eddd
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-08-30 23:44:23 +0530

    Time: 0 ms (100.00%) | Memory: 35.3 MB (5.14%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,55 +1,63 @@
 class Solution {
-    int dx[4]={1,-1,0,0};
-    int dy[4]={0,0,1,-1};
-    vector<vector<int>>vis;
 public:
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-       vis= vector<vector<int>>(m,vector<int>(n,0));
-
-        vector<int> size_of_comp;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis[i][j]  && grid[i][j] == 1){
-                    int cnt=0;
-                    dfs({i,j},cnt,m,n,grid);
-                    size_of_comp.push_back(cnt);
-                }
-            }
-        }
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<pair<int,int>>>comp;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         int ans=0;
-        for(auto i: size_of_comp){
-            ans=max(ans,i);
-        }
-        return ans;
-        
-        
-    }
-    void dfs(pair<int,int> st,int& cnt,int m,int n,vector<vector<int>>&grid){
-        vis[st.first][st.second]=1;
-        cnt++;
-        for(int i=0;i<4;i++){
-            int x=st.first+dx[i];
-            int y=st.second+dy[i];
-            if(check(x,y,m,n,grid)){
-                if(!vis[x][y]){
-                    
-                    dfs({x,y},cnt,m,n,grid);
 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+              
+
+                if(grid[i][j]==1 && vis[i][j]==0){
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    vis[i][j]=1;
+                      vector<pair<int,int>>st;
+            
+
+
+                    while(!q.empty()){
+                        auto [x,y]=q.front();
+                       st.push_back({x,y});
+
+                        q.pop();
+
+                        for(int i=0;i<4;i++){
+                            int X=x+dx[i];
+                            int Y=y+dy[i];
+                            if(X<0 || Y<0 || X>=n || Y>=m || grid[X][Y]==0 || vis[X][Y]==1)continue;
+
+                            if(grid[X][Y]==1 && vis[X][Y]==0){
+                                vis[X][Y]=1;
+                                q.push({X,Y});
+
+                            }
+
+                        }
+                    }
+                    comp.push_back(st);
+                   
 
                 }
+
+               
             }
         }
-
-    }
-    bool check(int x,int y,int m,int n,vector<vector<int>>&grid){
-           if(x<0 || y<0|| y>=n|| x>=m || grid[x][y]==0){
-            return 0;
+        for(auto i:  comp){
+            int x=i.size();
+            ans=max(x,ans);
         }
-        return 1;
 
+        return ans;
 
+        
 
+        
     }
 };
