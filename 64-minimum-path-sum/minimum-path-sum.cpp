60cbf54a192fb6e3f9c4b4964cd50ad365a6897b
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-09-10 08:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (15.93%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,54 +1,22 @@
 class Solution {
 public:
+vector<vector<int>>dp;
+int n,m;
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        n=grid.size();
+        m=grid[0].size();
+        dp.assign(n+1,vector<int>(m+1,-1));
+       return rec(0,0,grid);
 
-        // Distance matrix
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        
+    }
+    int rec(int i,int j,vector<vector<int>>& grid){
+        if(i==n-1 && j==m-1)return grid[i][j];
+        if(i>=n || j>=m) return 1e5;
+        if(dp[i][j]!=-1)return dp[i][j];
 
-        // Min-heap: {cost, {row, col}}
-        priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<>
-        > pq;
-
-        // Start from (0,0)
-        dist[0][0] = grid[0][0];
-        pq.push({grid[0][0], {0, 0}});
-
-        int dr[2] = {0, 1}; // right, down
-        int dc[2] = {1, 0};
-
-        while (!pq.empty()) {
-            auto [cost, cell] = pq.top();
-            pq.pop();
-
-            int r = cell.first;
-            int c = cell.second;
-
-            // Early exit if reached destination
-            if (r == m - 1 && c == n - 1)
-                return cost;
-
-            if (cost > dist[r][c]) continue;
-
-            for (int k = 0; k < 2; k++) {
-                int nr = r + dr[k];
-                int nc = c + dc[k];
-
-                if (nr < m && nc < n) {
-                    int newCost = cost + grid[nr][nc];
-
-                    if (newCost < dist[nr][nc]) {
-                        dist[nr][nc] = newCost;
-                        pq.push({newCost, {nr, nc}});
-                    }
-                }
-            }
-        }
-
-        return dist[m-1][n-1];
+        int ans=INT_MAX;
+        ans=min({ans,rec(i+1,j,grid)+grid[i][j],rec(i,j+1,grid)+grid[i][j]});
+        return dp[i][j]=ans;
     }
 };
