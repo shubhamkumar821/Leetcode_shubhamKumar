c125d3247f3e239a5e8c22028da38a33764bc2db
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-09-13 01:36:19 +0530

    Time: 50 ms (0.27%) | Memory: 50.1 MB (6.12%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-  vector<int> dp;
+vector<vector<int>>dp;
     int rob(vector<int>& nums) {
-      
-        dp.resize(nums.size(),-1);
-        
-        int myans=rec(0,nums);
-        return myans;
+      int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        dp.assign(nums.size()+1,vector<int>(sum+1,-1));
+        return rec(0,0,nums);
+
+
+
         
     }
-    int rec(int level,vector<int>& nums){
-        if(level>=nums.size()){
-            return 0;
-        }
+    int rec(int i,int sum,vector<int>&nums){
+        if(i>=nums.size())return 0;
+        if(dp[i][sum]!=-1)return dp[i][sum];
 
-        int ans=rec(level+1,nums);
-        if(dp[level]!=-1){
-            return dp[level];
-        }
-        ans=max(ans,nums[level]+rec(level+2,nums));
-        dp[level]=ans;
-        return ans;
+
+        int ans=INT_MIN;
+        int take=rec(i+2,sum+nums[i],nums)+nums[i];
+        int dtake=rec(i+1,sum,nums);
+
+        ans=max(take,dtake);
+
+      return  dp[i][sum]=ans;
+
+
     }
 };
