6d8de31c3c1f49e8712befc27f73f34dd85e34ee
shubhamkumar821 <153154895+shubhamkumar821@users.noreply.github.com>
2026-09-25 12:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (49.83%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -8,10 +8,10 @@ public:
         int left = max(0, rec(node->left));
         int right = max(0, rec(node->right));
 
-        // path passing through current node
+       
         ans = max(ans, left + right + node->val);
 
-        // return best downward path
+   
         return node->val + max(left, right);
     }
 
